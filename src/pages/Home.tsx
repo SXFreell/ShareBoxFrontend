198112343f5ba1a753946da779a79e6cf440b4fc
SXFreell <xfreell@163.com>
2023-12-22 18:32:23 +0800

    feat: 增加过期时间以及区间次数设置

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,37 +1,104 @@
-import { Button, Input, Radio, RadioGroup, Space, TextArea } from "@douyinfe/semi-ui"
+import { Button, DatePicker, Input, InputNumber, Radio, RadioGroup, Select, Space, TextArea, Toast } from "@douyinfe/semi-ui"
 import { MouseEventHandler, useEffect, useState } from "react"
 import '@css/home.less'
 import { NavigateFunction, useLocation, useNavigate } from "react-router-dom";
 import instance from "@/net/axios.tsx";
 
+interface MaxAndMin {
+    max: number,
+    min: number
+}
+
+interface DateMaxAndMin {
+    years: MaxAndMin,
+    months: MaxAndMin,
+    days: MaxAndMin,
+    hours: MaxAndMin,
+    minutes: MaxAndMin,
+    seconds: MaxAndMin
+}
+
+type ExpiresType = 'never-expires' | 'expiration-time' | 'countdown' | 'pickup-count'
+type CountDateType = 'years' | 'months' | 'days' | 'hours' | 'minutes' | 'seconds'
+
 const useQuery = () => {
     return new URLSearchParams(useLocation().search);
+}
+
+const disabledDate = (current: any) => {
+    const now = new Date();
+    now.setHours(0, 0, 0, 0);
+    return current && current < now;
+}
+
+const dateMaxAndMin: DateMaxAndMin = {
+    years: {max: 100, min: 1},
+    months: {max: 1200, min: 1},
+    days: {max: 36500, min: 1},
+    hours: {max: 24*36500, min: 1},
+    minutes: {max: 60*24*36500, min: 1},
+    seconds: {max: 60*60*24*36500, min: 1}
+}
+
+const verCode: Function = (code: string, get: boolean = false): boolean => {
+    // 以一位大小写字母开头，0个或多个数字结尾，数字不超过5个
+    if (code.length === 0) return true;
+    const reg = get ? /^[a-zA-Z]\d{5}$/ : /^[a-zA-Z]\d{0,5}$/;
+    return reg.test(code);
+}
+
+const transTimestamp: Function = (
+    expiresType: ExpiresType,
+    countDateType: CountDateType,
+    countDate: number,
+    date: number
+): number => {
+    switch (expiresType) {
+        case 'never-expires':
+            return -1;
+        case 'expiration-time':
+            return date;
+        case 'countdown':
+            const timestamp = Date.now();
+            switch (countDateType) {
+                case 'years':
+                    return timestamp + 1000 * 60 * 60 * 24 * 365 * countDate;
+                case 'months':
+                    return timestamp + 1000 * 60 * 60 * 24 * 30 * countDate;
+                case 'days':
+                    return timestamp + 1000 * 60 * 60 * 24 * countDate;
+                case 'hours':
+                    return timestamp + 1000 * 60 * 60 * countDate;
+                case 'minutes':
+                    return timestamp + 1000 * 60 * countDate;
+                case 'seconds':
+                    return timestamp + 1000 * countDate;
+            }
+            break;
+        case 'pickup-count':
+            return -1;
+    }
+    return -1;
 }
 
 const Home = () => {
     const query = useQuery();
     const getCode: string | null = query.get('code');
-    const navigate: NavigateFunction = useNavigate()
-
-    // useEffect(() => {
-    //     navigate('/login')
-    // })
+    const navigate: NavigateFunction = useNavigate();
 
     const [doWhat, setDoWhat] = useState<string>('get');
+
+    // Get
     const [code, setCode] = useState<string>('');
-    const [textFile, setTextFile] = useState<string>('')
     const [getTextFile, setGetTextFile] = useState<string>('')
 
-    // const handleClick: MouseEventHandler = () => {
-    //     console.log('doWhat', doWhat)
-    // }
-
-    const verCode: Function = (code: string, get: boolean = false): boolean => {
-        // 以一位大小写字母开头，0个或多个数字结尾，数字不超过5个
-        if (code.length === 0) return true;
-        const reg = get ? /^[a-zA-Z]\d{5}$/ : /^[a-zA-Z]\d{0,5}$/;
-        return reg.test(code);
-    }
+    // Set
+    const [expiresType, setExpiresType] = useState<ExpiresType>('never-expires')
+    const [expirationTime, setExpirationTime] = useState<number>(Date.now())
+    const [countDateType, setCountDateType] = useState<CountDateType>('days')
+    const [countDate, setCountDate] = useState<number>(1)
+    const [countdown, setCountdown] = useState<number>(1)
+    const [textFile, setTextFile] = useState<string>('')
 
     const handleGetTextFile: MouseEventHandler = () => {
         if (!verCode(code, true)) return;
@@ -39,7 +106,9 @@ const Home = () => {
             code: code
         }).then((res: any) => {
             setGetTextFile(res.data.data.content)
-            console.log(res)
+            Toast.success({
+                content: '取件成功'
+            })
         }).catch((err: any) => {
             console.log(err)
         })
@@ -50,19 +119,15 @@ const Home = () => {
             type: 'TEXT',
             set_text_content: {
                 content: textFile,
-                expires: -1
+                expires: parseInt(transTimestamp(expiresType, countDateType, countDate, expirationTime)),
             }
-        }).then((res: any) => {
-            console.log(res)
+        }).then(() => {
+            Toast.success({
+                content: '保存成功'
+            })
         }).catch((err: any) => {
             console.log(err)
         })
-        // const blob = new Blob([textFile], {type: 'text/plain'})
-        // const a = document.createElement('a')
-        // a.download = 'text.txt'
-        // a.href = URL.createObjectURL(blob)
-        // a.click()
-        // URL.revokeObjectURL(a.href)
     }
 
     useEffect(() => {
@@ -110,6 +175,68 @@ const Home = () => {
                         />
                     </Space> :
                     <Space vertical align="start" style={{width: '100%'}}>
+                        <Space>
+                            <Select
+                                defaultValue={"never-expires"}
+                                value={expiresType}
+                                onChange={(value: any) => {setExpiresType(value)}}
+                                style={{width: 120}}
+                            >
+                                <Select.Option value={"never-expires"}>永不过期</Select.Option>
+                                <Select.Option value={"expiration-time"}>过期时间</Select.Option>
+                                <Select.Option value={"countdown"}>倒计时</Select.Option>
+                                <Select.Option value={"pickup-count"}>取件次数</Select.Option>
+                            </Select>
+                            {
+                                expiresType === 'expiration-time' ?
+                                    <DatePicker
+                                        type="dateTime"
+                                        placeholder="过期时间"
+                                        disabledDate={disabledDate}
+                                        hideDisabledOptions
+                                        value={expirationTime}
+                                        onChange={(date: any) => {setExpirationTime(date)}}
+                                    /> :
+                                expiresType === 'countdown' ?
+                                    <>
+                                        <InputNumber
+                                            placeholder="倒计时"
+                                            min={dateMaxAndMin[countDateType].min}
+                                            max={dateMaxAndMin[countDateType].max}
+                                            style={{width: 200}}
+                                            value={countDate}
+                                            onChange={(value: any) => {setCountDate(value)}}
+                                        /> 
+                                        <Select
+                                            defaultValue={"days"}
+                                            value={countDateType}
+                                            onChange={(value: any) => {
+                                                setCountDateType(value)
+                                                setCountDate(dateMaxAndMin[value as CountDateType].min)
+                                            }}
+                                            style={{width: 64}}
+                                        >
+                                            <Select.Option value={"years"}>年</Select.Option>
+                                            <Select.Option value={"months"}>月</Select.Option>
+                                            <Select.Option value={"days"}>天</Select.Option>
+                                            <Select.Option value={"hours"}>时</Select.Option>
+                                            <Select.Option value={"minutes"}>分</Select.Option>
+                                            <Select.Option value={"seconds"}>秒</Select.Option>
+                                        </Select>
+                                    </> :
+                                expiresType === 'pickup-count' ?
+                                    <InputNumber
+                                        placeholder="取件次数"
+                                        min={1}
+                                        max={99999}
+                                        style={{width: 200}}
+                                        suffix={"次"}
+                                        value={countdown}
+                                        onChange={(value: any) => {setCountdown(value)}}
+                                    /> :
+                                null
+                            }
+                        </Space>
                         <TextArea
                             autosize
                             maxCount={200000}
